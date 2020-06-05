86c03fcad88f697640815b321ce75ddfe26ee7b0
InMapleWoods <llf99723@gmail.com>
2020-06-05 10:23:57 +0800

    6/5 添加流通子系统窗体

--- 项目汇总/程序代码/LibraryManagement/LibraryManagement/Periodical/PeriodicalForm.Designer.cs
+++ 项目汇总/程序代码/LibraryManagement/LibraryManagement/Periodical/PeriodicalForm.Designer.cs
@@ -31,13 +31,12 @@
             this.orderButton = new System.Windows.Forms.Button();
             this.arriveButton = new System.Windows.Forms.Button();
             this.bindiButton = new System.Windows.Forms.Button();
-            this.orderModificationButton = new System.Windows.Forms.Button();
             this.reportFormButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // orderButton
             // 
-            this.orderButton.Location = new System.Drawing.Point(49, 84);
+            this.orderButton.Location = new System.Drawing.Point(120, 101);
             this.orderButton.Name = "orderButton";
             this.orderButton.Size = new System.Drawing.Size(100, 100);
             this.orderButton.TabIndex = 0;
@@ -47,7 +46,7 @@
             // 
             // arriveButton
             // 
-            this.arriveButton.Location = new System.Drawing.Point(330, 84);
+            this.arriveButton.Location = new System.Drawing.Point(277, 101);
             this.arriveButton.Name = "arriveButton";
             this.arriveButton.Size = new System.Drawing.Size(94, 100);
             this.arriveButton.TabIndex = 2;
@@ -56,25 +55,16 @@
             // 
             // bindiButton
             // 
-            this.bindiButton.Location = new System.Drawing.Point(471, 84);
+            this.bindiButton.Location = new System.Drawing.Point(437, 101);
             this.bindiButton.Name = "bindiButton";
             this.bindiButton.Size = new System.Drawing.Size(94, 100);
             this.bindiButton.TabIndex = 3;
             this.bindiButton.Text = "期刊装订";
             this.bindiButton.UseVisualStyleBackColor = true;
             // 
-            // orderModificationButton
-            // 
-            this.orderModificationButton.Location = new System.Drawing.Point(185, 84);
-            this.orderModificationButton.Name = "orderModificationButton";
-            this.orderModificationButton.Size = new System.Drawing.Size(94, 100);
-            this.orderModificationButton.TabIndex = 1;
-            this.orderModificationButton.Text = "订单修改";
-            this.orderModificationButton.UseVisualStyleBackColor = true;
-            // 
             // reportFormButton
             // 
-            this.reportFormButton.Location = new System.Drawing.Point(610, 84);
+            this.reportFormButton.Location = new System.Drawing.Point(589, 101);
             this.reportFormButton.Name = "reportFormButton";
             this.reportFormButton.Size = new System.Drawing.Size(94, 100);
             this.reportFormButton.TabIndex = 4;
@@ -86,14 +76,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Controls.Add(this.orderModificationButton);
             this.Controls.Add(this.reportFormButton);
             this.Controls.Add(this.bindiButton);
             this.Controls.Add(this.arriveButton);
             this.Controls.Add(this.orderButton);
             this.Name = "PeriodicalForm";
             this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "期刊子系统";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PeriodicalForm_FormClosing);
             this.ResumeLayout(false);
@@ -105,7 +94,6 @@
         private System.Windows.Forms.Button orderButton;
         private System.Windows.Forms.Button arriveButton;
         private System.Windows.Forms.Button bindiButton;
-        private System.Windows.Forms.Button orderModificationButton;
         private System.Windows.Forms.Button reportFormButton;
     }
 }
