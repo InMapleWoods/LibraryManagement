4ae9dbf00fdef19049bb796d9d3a6c7c518e069c
lNEWBEEl <823386956@qq.com>
2020-06-08 17:46:01 +0800

    采访子系统设计

--- 项目汇总/程序代码/LibraryManagement/LibraryManagement/Interview/BookOrderForm.cs
+++ 项目汇总/程序代码/LibraryManagement/LibraryManagement/Interview/BookOrderForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibraryManagement.Tools.MyUserControl;
+using LibraryManagement.Model;
 
 namespace LibraryManagement.Interview
 {
@@ -17,6 +19,31 @@ namespace LibraryManagement.Interview
         {
             InitializeComponent();
             parentForm = form;
+            scriptUserControl1.AddContorlClickMethod(ExitLog_Click,
+                ScriptUserControl.ControlNames.exitButton);
+            scriptUserControl1.AddContorlClickMethod(AddLog_Click,
+                ScriptUserControl.ControlNames.addButton);
+        }
+
+        private void AddLog_Click(object sender,EventArgs e)
+        {
+            try
+            {
+                List<string> errorList = new List<string>();//创建一个错误列表
+                //获取根据当前页面内容生成的订单（若有错误会被添加到错误列表中）
+                PurchaseOrder order = Get
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ExitLog_Click(object sender,EventArgs e)
+        {
+            parentForm.Show();
+            Hide();
         }
 
         private void Label1_Click(object sender, EventArgs e)
