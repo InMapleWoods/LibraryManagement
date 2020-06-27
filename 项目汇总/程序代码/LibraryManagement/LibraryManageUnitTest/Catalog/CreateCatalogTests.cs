1557a78aa42ce34c34258cddcf283fed53c97a6d
LoveLisa <3139489078@qq.com>
2020-06-27 18:51:50 +0800

    6/27 单元测试

---
+++ 项目汇总/程序代码/LibraryManagement/LibraryManageUnitTest/Catalog/CreateCatalogTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryManagement.Bll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Bll.Tests
+{
+    [TestClass()]
+    public class CreateCatalogBllTests
+    {
+        [TestMethod()]
+        public void AddCatalogListTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void DeleteCatalogListTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void UpdateCatalogListTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void AddInterviewCatalogTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void DeleteInterviewCatalogTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void UpdateInterviewCatalogTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MoveInterviewCatalogToCirculateTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void MoveCatalogTest()
+        {
+
+        }
+    }
+}
